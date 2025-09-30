074f32254519859c66634429308a3397c0cc0960
elchemista <elchemista@gmail.com>
2025-09-30 06:38:04 +0200

    Fix Tailwind content paths for asset build

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -3,7 +3,8 @@ const path = require("path");
 
 module.exports = {
   content: [
-    path.join(__dirname, "web/pages/**/*.{html,tmpl}"),
+    path.join(__dirname, "../web/pages/**/*.{html,tmpl}"),
+    path.join(__dirname, "./src/**/*.{js,ts}"),
   ],
   theme: {},
   plugins: [],
